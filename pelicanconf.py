d547d049fdc67691f28a4f8c68077c3c2dd6ce53
Francisco H C Felix <fhcflx@outlook.com>
2016-09-08 10:26:37 -0300

    theme configs

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,13 +24,24 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#        ('You can modify those links in your config file', '#'),
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+#         ('You can add links in your config file', '#'),
+#         ('Another social link', '#'),
+          ('google-plus', 'https://plus.google.com/u/0/+FranciscoHCFelix'),
+          ('twitter', 'http://twitter.com/fhcflx'),
+          ('github', 'http://github.com/fhcflx'),
+          ('gitlab', 'http://gitlab.com/fhcflx'),
+          ('bitbucket', 'http://bitbucket.org/fhcflx'),
+          )
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+
+# Specify name of a built-in theme
+THEME = "mytheme"
